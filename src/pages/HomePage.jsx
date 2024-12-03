3e5101805c0b7c473bfcff833d014d7e48b95a92
FauzhanWahyudi <fauzhanwahyudi@gmail.com>
2024-12-03 14:06:41 +0700

    fix: socket looping by adding socket hooks

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,21 @@
 import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
+import { useSocket } from "../hooks/useSocket";
 
 export default function HomePage() {
   const [message, setMessage] = useState("");
   const [points, setPoints] = useState([]);
+  const socket = useSocket();
 
-  const socket = io("http://localhost:3000/");
   const submitHandler = (e) => {
     e.preventDefault();
-    socket.emit("player/answer", message);
+    socket?.emit("player/answer", { message });
     setMessage("");
   };
   useEffect(() => {
-    socket.on("player/points", (args) => {
-      setPoints(args);
-      return () => {
-        socket.disconnect();
-      };
+    socket?.on("player/points", (points) => {
+      setPoints(points);
     });
-  }, []);
+  }, [socket]);
 
   return (
     <>
